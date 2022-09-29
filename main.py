b56767ffffec09d67c414e15885e42595185e875
Seth Guimont <sethguimont@pop-os.localdomain>
2022-09-28 19:18:31 -0500

    groundwork

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ def main():
     form = BasicForm()
     return render_template("index.html", form=form)
 
+'''
+Code will go here that routes a person to a specific bucket
+Depending on state
+if hit wisconsin button go to wisconsin bucket and etc....
+They will be different forms attached to different buckets
+'''
+@app.route('/minnesota', methods=['POST', 'GET'])
+def minnesota():
+    min_form = BasicForm()
+    return render_template('minnesota.html')
+
+@app.route('/wisconsin', methods=['POST', 'GET'])
+def wisconsin():
+    wis_form = BasicForm()
+    return render_template('wisconsin.html')
+
 
 if __name__ == '__main__':
     app.run()
