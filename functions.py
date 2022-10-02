d660184639fb9b61e4fb3e08986339b9065f9ed0
Seth Guimont <sethguimont@pop-os.localdomain>
2022-10-02 12:06:49 -0500

    adding testing capabilities

--- functions.py
+++ functions.py
@@ -1,8 +1,21 @@
+import json
 import os
 import boto3
 
 
-def upload_file(file_name, bucket):
+def download_json_files(bucket: str, prefix: str, local_dir: str) -> None:
+    bucket = boto3.resource("s3").Bucket(bucket)
+    objects = bucket.objects.filter(Prefix=prefix)
+    keys = [obj.key for obj in objects if obj.key.endswith(".json")]
+
+    local_paths = [os.path.join(local_dir, key) for key in keys]
+
+    for key, local_path in zip(keys, local_paths):
+        os.makedirs(os.path.dirname(local_path), exist_ok=True)
+        bucket.download_file(key, local_path)
+
+
+'''def upload_file(file_name, bucket):
     """
     Function to upload a file to an S3 bucket
     """
@@ -33,4 +46,4 @@ def list_files(bucket):
     for item in s3.list_objects(Bucket=bucket)['Contents']:
         contents.append(item)
 
-    return contents
+    return contents'''
