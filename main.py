c0e6fd9c2a336dc2e1391ccea87da7f95cc8b6f4
Seth Guimont <sethguimont@outlook.com>
2022-10-30 16:05:27 -0500

    fixed fututure test issues and logic for reading forms from bucket

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import os
 import boto3
-from flask import Flask, render_template, request, url_for, flash, redirect
+#import boto3
+from flask import Flask, render_template, request, url_for, flash, redirect, flash, \
+    Response, session
 from wtforms import Form, StringField, SubmitField
 from wtforms.validators import DataRequired
+from werkzeug.utils import secure_filename
 
 # s3 = boto3.client('')
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+
+BUCKET_NAME = 'medformmadeeasy'
 
 
 class BasicForm(Form):
@@ -47,6 +54,42 @@ def about():
 @app.route('/services')
 def services():
     return render_template('services.html')
+
+
+'''
+Routes below are for forms and listing off s3 contents
+'''
+
+
+@app.route('/files')
+def files():
+    my_bucket = BUCKET_NAME
+    #summeries = my_bucket.objects.all()
+    return render_template('files.html', my_bucket=my_bucket) #, files=summeries)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    my_bucket = BUCKET_NAME
+    my_bucket.Object(file.filename).put(Body=file)
+    flash('File uploaded successfully')
+    return redirect(url_for('files'))
+
+
+@app.route('/download', methods=['POST'])
+def download():
+    key = request.form['key']
+
+    s3_resource = boto3.resource('s3')
+    my_bucket = BUCKET_NAME
+
+    file_obj = my_bucket.Object(key).get()
+
+    return Response(
+        file_obj['BODY'].read(),
+        mimetype='text/plain',
+        headers={"Content-Disposition": "attachment;filename={}".format(key)}
+    )
 
 
 if __name__ == '__main__':
--- tests/first_automation.py
+++ tests/first_automation.py
@@ -1,15 +1,9 @@
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+driver = webdriver.Firefox()
 
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get("127.0.0.1:5000")
+#assert "Python" in driver.title
 driver.close()
