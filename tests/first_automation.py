49d08db2beec6bdf823b0004d75f932ddbcc5ff2
Seth Guimont <sethguimont@outlook.com>
2022-11-26 15:36:39 -0600

    fixing title of pages

--- tests/first_automation.py
+++ tests/first_automation.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import unittest
 
 driver = webdriver.Firefox()
 
 driver.get("127.0.0.1:5000")
 #assert "Python" in driver.title
 driver.close()
+
+
+
+
+
+
+
+# https://www.geeksforgeeks.org/locating-single-elements-in-selenium-python/
