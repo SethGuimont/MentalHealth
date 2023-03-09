330cd59b070e4c33e60a9073fb652f7fecc1b89d
Seth Guimont <sethguimont@outlook.com>
2023-03-08 19:34:18 -0600

    added unit tests

---
+++ unit_tests.py
@@ -0,0 +1,33 @@
+import pytest
+from main import *
+
+
+@pytest.fixture
+def client():
+    """Configures the app for testing
+
+    Sets app config variable ``TESTING`` to ``True``
+
+    :return: App for testing
+    """
+
+    # app.config['TESTING'] = True
+    client = app.test_client()
+
+    yield client
+
+
+def test_landing(client):
+    landing = client.get("/")
+    html = landing.data.decode()
+
+    assert "Select The State You Live In" in html
+    assert landing.status_code == 200
+
+
+def test_filedisplay(client):
+    aws_files = client.get('/files')
+    html = aws_files.data.decode()
+
+    assert "Click on the filename to download it" in html
+    assert aws_files.status_code == 200
